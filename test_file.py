845870d744e8ad914d3787ee10f624b5c8db5140
Galen Richard <richardg@localhost.localdomain>
2018-03-03 15:40:20 -0500

    pygame queue for sounds does not seem to be working...

--- test_file.py
+++ test_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
-
 import pygame
 from pygame.locals import *
+
+# import system modules
+from glob import glob
 
 def main():
     # initialize screen
@@ -25,10 +27,24 @@ def main():
     screen.blit(background, (0, 0))
     pygame.display.flip()
 
+    count = False
+    list = [ i for i in glob("/usr/share/sounds/gnome/default/alerts/*.ogg")]
+
+    pygame.mixer.init()
+    pygame.mixer.music.load(list[0])
+    pygame.mixer.music.play()
+
+    for i in list[1:]:
+        pygame.mixer.music.queue(i)
+
+    # queue doesn't seem to be working
+    # pygame.mixer.music.queue("/usr/share/sounds/gnome/default/alerts/bark.ogg")
+
     # Event loop
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
+                pygame.mixer.music.stop()
                 return
 
         screen.blit(background, (0, 0))
